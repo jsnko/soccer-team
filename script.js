9a06cf364e382046208a35c468ac4fe57f96f852
Jasenko <121662342+jsnko@users.noreply.github.com>
2023-03-06 18:25:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const goalie = [
+  "Sam"
+]
+
+const defenders = [
+  "Lisa",
+  "Tom",
+  "Jenny",
+  "Jo"
+];
+
+const midfielders = [
+  "Jim",
+  "Kai",
+  "Ian",
+  "Ava",
+]
+
+const forwards = [
+  "Kim",
+  "Lucas"
+]
+
+console.log("Goalie: " + goalie[0]);
+console.log("Defenders: " + defenders);
+console.log("Midfielders: " + midfielders);
+console.log("Forwards: " + forwards);
